f752737aaa8e72f6fa8e7b8d4b65ecb72356e0a6
Jia Fangtao <fangtao.jia@peraglobal.com>
2023-08-23 14:12:53 +0800

    Task 2 - iterate thru all solid shapes
    How can I get extra data for solids?

---
+++ mysandbox/README.md
@@ -0,0 +1,6 @@
+# Tasks
+
+1. Task 1: Read and show an stp file, and export it to glTF.
+1. Task 2: Dump an stp file
+1. Task 3: Create primitives (box, cylinder, sphere, etc.) and move/remote them (Ongoing)
+1. Task 4: Add a box to an existing stp and save to a new stp file.
--- mysandbox/task2.py
+++ mysandbox/task2.py
@@ -5,11 +5,19 @@
 #
 
 from OCC.Extend.DataExchange import read_step_file
-from OCC.Extend.TopologyUtils import TopologyExplorer
+from OCC.Extend.TopologyUtils import (
+    TopologyExplorer,
+    get_type_as_string,
+    dump_topology_to_string
+)
 
-def main():
-    pass
+def main(file_path):
+    shp = read_step_file(file_path)
+    topo = TopologyExplorer(shp)
+    for solid in topo.solids():
+        print(get_type_as_string(solid))
 
+    dump_topology_to_string(shp)
 
 if __name__ == '__main__':
-    main()
+    main("dianzijixiang.stp")
