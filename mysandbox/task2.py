e44d444e38e78cd5453a9c82a9a40fb0a37812b7
jiafangtao <jiafangtao@gmail.com>
2024-08-07 16:44:58 +0800

    wrap up for now

--- mysandbox/task2.py
+++ mysandbox/task2.py
@@ -6,9 +6,16 @@
 
 from OCC.Extend.DataExchange import read_step_file
 from OCC.Extend.TopologyUtils import TopologyExplorer
+from OCC.Extend.DataExchange import read_step_file
+
 
 def main():
     pass
+
+def import_as_multiple_shapes(filename: str):
+    compound = read_step_file(filename)
+    t = TopologyExplorer(compound)
+    return t.solids()
 
 
 if __name__ == '__main__':
